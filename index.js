f96a9c02200343b19a9c9e0eb8e6dfddf14403f5
viyan-najmaldin <najmadinviyan@gmail.com>
2022-09-02 17:22:21 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+
+let currentUser = "viyan";
+
+let welcomeMessage = "Welcome to Flatbook, ";
+welcomeMessage = `Welcome to Flatbook, !`;
+
+currentUser = welcomeMessage;
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, !";
+
+let shortGreeting = "Welcome, !";
